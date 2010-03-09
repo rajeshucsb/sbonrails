7a741bd3b2b779cd4743ad0dc86e904c4552727e
Joshua Carver <jcarver989@gmail.com>
2010-03-08 16:18:44 -0800

    configured mailer for heroku

--- config/environment.rb
+++ config/environment.rb
@@ -9,14 +9,3 @@ Rails::Initializer.run do |config|
   config.time_zone = 'UTC'
 end
 
-
-ActionMailer::Base.smtp_settings = {
-    :enable_starttls_auto => true,
-    :address => "smtp.gmail.com",
-    :port => "587",
-    :domain => "gmail.com",
-    :authentication => :plain,
-    :user_name => "santabarbara.railsgroup",
-    :password => "rUb!ypR0G8rm"
-  }
-
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,3 +16,14 @@ config.action_controller.perform_caching             = false
 config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 config.action_mailer.raise_delivery_errors = true
 
+
+ActionMailer::Base.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :domain => "gmail.com",
+    :authentication => :plain,
+    :user_name => "santabarbara.railsgroup",
+    :password => "rUb!ypR0G8rm"
+  }
+
