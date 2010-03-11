2aa09068fbb1dfbb45e68cd0a35de90c4ecf0bdb
Joshua Carver <jcarver989@gmail.com>
2010-03-10 23:13:26 -0800

    added helpers for buttons to hide extra <span> tags wrapping link_to text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,6 +10,16 @@ module ApplicationHelper
   end
 
 
+  def large_button(text,url,options)
+    options[:class] = "large #{options[:color]} button"
+    link_to "<span>#{text} &raquo;</span>",url,options
+  end
+
+  def small_button(text,url,options)
+    options[:class] = "small #{options[:color]} button"
+    link_to "<span>#{text}</span>",url,options
+  end
+
 
 
 end
