b8f5c29d2b4073455627772ba610826c4e0d9bd4
Joshua Carver <jcarver989@gmail.com>
2010-03-18 18:26:29 -0700

    forgot to include the js file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,6 +15,11 @@ module ApplicationHelper
     link_to "<span>#{text} &raquo;</span>",url,options
   end
 
+  def large_pulsating_button(text,url,options)
+    options[:class] = "large #{options[:color]} pulsating button"
+    link_to "<span>#{text} &raquo;</span>",url,options
+  end
+
   def small_button(text,url,options)
     options[:class] = "small #{options[:color]} button"
     link_to "<span>#{text}</span>",url,options
